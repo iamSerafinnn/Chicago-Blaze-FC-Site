3e3adac61f5d36751de054a559c115a0a92aa856
Serafin O. Gargantiel III <gargantielserafin@gmail.com>
2026-08-21 13:41:34 -0500

    Update base path to match actual repo name Chicago-Blaze-FC-Site

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/chicago-blaze-site/',
+  base: '/Chicago-Blaze-FC-Site/',
   plugins: [react()],
 })
